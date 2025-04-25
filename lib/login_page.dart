67e707ab4fb35608aff34ea6bcf2fa928b843dd6
isro24 <alamat_email_anda@example.com>
2025-04-25 10:23:21 +0700

    Membuat TextFormField pada Login Page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,8 +8,86 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();
+    return  Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Selamat Datang Kembali',
+                style: TextStyle(
+                  fontSize: 27, fontWeight: FontWeight.w500),),
+              const SizedBox(height: 30),
+              Text('Email'),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4),
+                    borderSide: BorderSide(color: Colors.amber)
+                  ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(
+                        Icons.email,
+                        color: Colors.black,
+                      ), 
+                    ),
+                  ),
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Email tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              Text('Password'),
+              TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                   border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4),
+                    borderSide: BorderSide(color: Colors.amber)
+                  ),
+                  prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Icon(
+                        Icons.lock,
+                        color: Colors.black,
+                      ), 
+                    ),
+                  ),
+                obscureText: true,
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Password tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 40),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
