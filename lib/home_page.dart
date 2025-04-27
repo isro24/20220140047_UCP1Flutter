4da6fe0290421958316bf4522c0dbbc75e648883
isro24 <alamat_email_anda@example.com>
2025-04-28 00:26:10 +0700

    Menambahkan Image dan Card Fitur aplikasi pada Home Page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ucp1/login_page.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomePage extends StatelessWidget {
   final String email;
@@ -45,18 +46,127 @@ class HomePage extends StatelessWidget {
               child: IconButton(
                 icon: Icon(Icons.logout, color: Colors.white),
                 onPressed: () {
-                  Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => LoginPage()));
+                  Navigator.pushAndRemoveUntil(
+                    context, MaterialPageRoute(builder: (context) => LoginPage()),
+                      (route) => false,
+                  );
                 }
               ),
             )
           ]
         ),
       body: Form(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(0),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/bg.webp'),
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ),
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 150,
+                      child: Card(
+                        color: Color.fromARGB(255, 255, 94, 0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(10),
+                          onTap: () {
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(MdiIcons.messageProcessingOutline, color: Colors.white, size: 40,),
+                              Text(
+                                'Data Piket',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      height: 150,
+                      child: Card(
+                        color: Color.fromARGB(255, 255, 94, 0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: InkWell(
+                          borderRadius: BorderRadius.circular(10),
+                          onTap: () {
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(MdiIcons.viewGridPlus, color: Colors.white, size: 40,),
+                              Text(
+                                'Data Pelanggan',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 150,
+                width: double.infinity,
+                child: Card(
+                  color: Color.fromARGB(255, 255, 94, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.receipt_long, color: Colors.white, size: 40,),
+                        Text(
+                          'Barang Masuk/Keluar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         )
       ),
     );
