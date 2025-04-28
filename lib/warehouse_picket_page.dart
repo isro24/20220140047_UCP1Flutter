cce282a177323f516e652c47f1e62168c7e604bb
isro24 <alamat_email_anda@example.com>
2025-04-28 16:24:30 +0700

    Menambah TextFieldForm dan Button pada Warehouse Picket Page

--- lib/warehouse_picket_page.dart
+++ lib/warehouse_picket_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class WarehousePicketPage extends StatefulWidget {
   const WarehousePicketPage({super.key});
@@ -26,6 +27,107 @@ class _WarehousePicketPageState extends State<WarehousePicketPage> {
           },
         ),
       ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Nama Anggota',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.amber),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Pilih Tanggal',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Pilih Tanggal',
+                  label: const Text('Pilih Tanggal'),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.amber),
+                  ),
+                  prefixIcon: Icon(
+                    MdiIcons.calendarCheck,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Tugas Piket',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Tugas Piket',
+                          label: const Text('Tugas Piket'),
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(color: Colors.amber),
+                          ),
+                        ),
+                      ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  SizedBox(
+                    width: 170,
+                    child: FilledButton(
+                      onPressed: (){
+                    
+                      }, 
+                      style: FilledButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(17),
+                      ),
+                      minimumSize: Size(170, 55),
+                      backgroundColor: Colors.orange[700]
+                    ),
+                      child: Text('Tambah'),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )),
     );
   }
 }
