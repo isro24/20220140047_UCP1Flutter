3ef7c44b1e8df3ebb903edc0753d94bcde1e74d0
isro24 <alamat_email_anda@example.com>
2025-04-28 20:06:34 +0700

    Menambah AppBar pada Item Page

--- lib/item_page.dart
+++ lib/item_page.dart
@@ -10,6 +10,22 @@ class ItemPage extends StatefulWidget {
 class _ItemPageState extends State<ItemPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pendataan Barang', 
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 94, 0),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
