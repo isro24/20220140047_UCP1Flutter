55c2a5350006776ef80fd4d12309801320b3dc9f
isro24 <alamat_email_anda@example.com>
2025-04-28 18:24:15 +0700

    Membuat Customer Data Page

---
+++ lib/customer_data_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomerDataPage extends StatefulWidget {
+  const CustomerDataPage({super.key});
+
+  @override
+  State<CustomerDataPage> createState() => _CustomerDataPageState();
+}
+
+class _CustomerDataPageState extends State<CustomerDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
