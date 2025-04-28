d7a289a2077039c6cbe5d7e1e4422699cc92d574
isro24 <alamat_email_anda@example.com>
2025-04-29 00:41:48 +0700

    Menambah AppBar  pada Detail Picket Page

--- lib/detail_picket_page.dart
+++ lib/detail_picket_page.dart
@@ -5,6 +5,22 @@ class DetailPicketPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data ', 
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 94, 0),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
