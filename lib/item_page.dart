d081299776f5562da1288d2b30b055da3f7d9980
isro24 <alamat_email_anda@example.com>
2025-04-28 20:36:46 +0700

    Menambah TextField dan Dropdown pada Item Page

--- lib/item_page.dart
+++ lib/item_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 class ItemPage extends StatefulWidget {
   const ItemPage({super.key});
 
@@ -25,6 +25,138 @@ class _ItemPageState extends State<ItemPage> {
             Navigator.pop(context);
           },
         ),
+      ),
+      body: Form(
+        child: Padding(
+          padding: EdgeInsets.all(16.0) ,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+               children: [
+                Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Tanggal Transaksi',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Tanggal Transaksi',
+                  label: const Text('Tanggal Transaksi'),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.amber),
+                  ),
+                  prefixIcon: Icon(
+                    MdiIcons.calendarCheck,
+                    color: Colors.grey,
+                  ),
+                ),
+                onTap: (){
+
+                },
+              ),
+              const SizedBox(height: 30),
+              DropdownButtonFormField(
+                decoration: InputDecoration(
+                  hintText: 'Jenis Transaksi',
+                  label: const Text('Jenis Transaksi'),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.amber),
+                  ),
+                ),
+                items: [
+                ],
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 30),
+              DropdownButtonFormField(
+                decoration: InputDecoration(
+                  hintText: 'Jenis Barang',
+                  label: const Text('Jenis Barang'),
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.amber),
+                  ),
+                ),
+                items: [
+                ],
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Jumlah Barang',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500),
+                          ),
+                        ), 
+                        const SizedBox(height: 13,),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Jumlah Barang',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelText: 'Jumlah Barang',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: Colors.amber)
+                            ),
+                            ),
+                        ),
+                      ],
+                    ) 
+                ),
+                const SizedBox(width: 10,),
+                Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Harga Satuan',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500),
+                          ),
+                        ), 
+                        const SizedBox(height: 13,),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Harga Satuan',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelText: 'Harga Satuan',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: Colors.amber)
+                            ),
+                            prefixIcon: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Rp.',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ) 
+                  ),
+                ],
+              ),
+          ],
+        ),
+      )
       ),
     );
   }
