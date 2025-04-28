d3e66b5a3d29436d50990c59237e4454d047691f
isro24 <alamat_email_anda@example.com>
2025-04-28 13:37:43 +0700

    Membuat Warehouse Picket Page

---
+++ lib/warehouse_picket_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WarehousePicketPage extends StatefulWidget {
+  const WarehousePicketPage({super.key});
+
+  @override
+  State<WarehousePicketPage> createState() => _WarehousePicketPageState();
+}
+
+class _WarehousePicketPageState extends State<WarehousePicketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
