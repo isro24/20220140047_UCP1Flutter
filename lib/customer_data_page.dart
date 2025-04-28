094b0dc5bcfb84647fecfb257c2f4d94c8ad44dd
isro24 <alamat_email_anda@example.com>
2025-04-28 19:06:58 +0700

    Menambah TextFormField pada Customer Data Page

--- lib/customer_data_page.dart
+++ lib/customer_data_page.dart
@@ -26,6 +26,169 @@ class _CustomerDataPageState extends State<CustomerDataPage> {
           },
         ),
       ),
+      body: Form(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text('Nama Cust',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500),
+                      ),
+                  ),
+                  const SizedBox(height: 13),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Nama Cust',
+                      label: Text('Nama Cust'),
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Email',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500),
+                          ),
+                        ), 
+                        const SizedBox(height: 13,),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelText: 'Email',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: Colors.amber)
+                            ),
+                            ),
+                        ),
+                      ],
+                    ) 
+                ),
+                const SizedBox(width: 15,),
+                Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('No Hp',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500),
+                          ),
+                        ), 
+                        const SizedBox(height: 13,),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'No HP',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelText: 'No HP',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: Colors.amber)
+                            ),
+                            ),
+                        ),
+                      ],
+                    ) 
+                ),
+              ],
+              ),
+              const SizedBox(height: 30),
+              Align(
+               alignment: Alignment.centerLeft,
+                    child: Text('Alamat',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500),
+                      ),
+                  ),
+                  const SizedBox(height: 13),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Alamat',
+                      label: Text('Alamat'),
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                ),
+              const SizedBox(height: 30),
+               Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Provinsi',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500),
+                          ),
+                        ), 
+                        const SizedBox(height: 13,),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Provinsi',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelText: 'Provinsi',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: Colors.amber)
+                            ),
+                            ),
+                        ),
+                      ],
+                    ) 
+                ),
+                const SizedBox(width: 15,),
+                Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Kode Pos',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500),
+                          ),
+                        ), 
+                        const SizedBox(height: 13,),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Kode Pos',
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            labelText: 'Kode Pos',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(color: Colors.amber)
+                            ),
+                            ),
+                        ),
+                      ],
+                    ) 
+                ),
+              ],
+              ),
+              ],
+            ),
+          ),
+      )
     );
   }
 }
