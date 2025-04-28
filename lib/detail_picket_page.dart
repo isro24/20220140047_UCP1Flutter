77c34a9ab71b19a74081aba5044db67c2d31b129
isro24 <alamat_email_anda@example.com>
2025-04-29 01:01:13 +0700

    Menambah data email, tanggal, dan tugas piket dari tugas piket pada Detail Piket Page

--- lib/detail_picket_page.dart
+++ lib/detail_picket_page.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 class DetailPicketPage extends StatelessWidget {
-  const DetailPicketPage({super.key});
+  
+  final String email;
+  final String tanggal;
+  final String tugasPiket;
+    
+  const DetailPicketPage({
+    super.key,
+    required this.email,
+    required this.tanggal,
+    required this.tugasPiket,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Data ', 
+        title: Text('Data $tugasPiket', 
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.w500),
@@ -19,6 +29,60 @@ class DetailPicketPage extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(tanggal,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.orange[800],
+                  ),
+                ),
+                SizedBox(width: 8),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.orange[800],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    email,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                color: Colors.orange[800],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(tugasPiket,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
