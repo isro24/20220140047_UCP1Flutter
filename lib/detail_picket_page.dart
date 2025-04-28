c2e45c2ebd96b3ca27c04f381090d4a688af1c88
isro24 <alamat_email_anda@example.com>
2025-04-28 23:35:56 +0700

    Membuat Detail Picket Page

---
+++ lib/detail_picket_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailPicketPage extends StatelessWidget {
+  const DetailPicketPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
