84696bc339d3679f9c34bcff90fe92b07a1c73c0
isro24 <alamat_email_anda@example.com>
2025-04-29 11:44:32 +0700

    Membuat Detail Item Page

---
+++ lib/detail_item_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailItemPage extends StatelessWidget {
+  const DetailItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
