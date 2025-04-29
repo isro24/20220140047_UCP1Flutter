3f73dd192fe7b0800dbc2878e81c610050fb056c
isro24 <alamat_email_anda@example.com>
2025-04-29 12:35:55 +0700

    Menambah detail data dari pendataan barang pada Detail Item Page

--- lib/detail_item_page.dart
+++ lib/detail_item_page.dart
@@ -1,10 +1,184 @@
 import 'package:flutter/material.dart';
 
 class DetailItemPage extends StatelessWidget {
-  const DetailItemPage({super.key});
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final String jumlahBarang;
+  final String jenisHargaSatuan;
+  final String totalHarga;
+
+  const DetailItemPage({
+    super.key,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlahBarang,
+    required this.jenisHargaSatuan,
+    required this.totalHarga,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 60),
+              Padding(
+                padding: EdgeInsets.all(2.0),
+                  child: Image.asset('assets/images/mark.png', height: 150, width:150),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Data Berhasil Disimpan',
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Tanggal',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(tanggal,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              Divider(
+                color: Colors.grey[200],
+                thickness: 2,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Jenis Transaksi',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(jenisTransaksi,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),],
+              ),
+               Divider(
+                color: Colors.grey[200],
+                thickness: 2,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Jenis Barang',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(jenisBarang,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+               Divider(
+                color: Colors.grey[200],
+                thickness: 2,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Jumlah Barang',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(jumlahBarang,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+               Divider(
+                color: Colors.grey[200],
+                thickness: 2,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Jenis Harga Satuan',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text('Rp. $jenisHargaSatuan',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+               Divider(
+                color: Colors.grey[200],
+                thickness: 2,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total Harga',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text('Rp. $totalHarga',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+               Divider(
+                color: Colors.grey[200],
+                thickness: 2,
+              ),
+              const SizedBox(height: 20),
+            ],    
+          ) 
+      ),
+    );
   }
 }
